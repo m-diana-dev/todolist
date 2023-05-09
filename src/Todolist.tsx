0129261ec7b354b05a9f159855e0e2b3103f9087
m-diana-dev <m.diana.dev@gmail.com>
2023-05-09 15:35:40 +0300

    monday-3

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
-import {ButtonNameType} from "./App";
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {FilterValuesType} from './App';
+import {Button} from "./components/Button";
 
 type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
@@ -10,48 +11,73 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: number, taskString: string)=>void
+    removeTask: (taskId: string) => void
+    addTask: (title: string) => void
+    changeFilter: (value: FilterValuesType) => void
 }
 
 export function Todolist(props: PropsType) {
+    const [title, setTitle] = useState('');
 
-    const [filteredTasks, setfilteredTasks] = useState<ButtonNameType>('All')
-
-    const filterTask = (buttonName: ButtonNameType) => {
-        setfilteredTasks(buttonName);
+    const addTaskHandler = () => {
+        props.addTask(title);
+        setTitle('');
     }
-    const boxForFilters = ()=>{
-        let durshlag = props.tasks;
-        if(filteredTasks === 'Active'){
-            durshlag = props.tasks.filter(el=> !el.isDone)
-        } else if (filteredTasks === 'Completed'){
-            durshlag = props.tasks.filter(el=> el.isDone)
+
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            addTaskHandler();
         }
-        return durshlag;
+    }
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value);
     }
 
+    // const removeTaskHandler = (tID: string) =>{
+    //     props.removeTask(tID)
+    // }
 
+    const changeFilterAll = () => {
+        props.changeFilter("all");
+    }
+    const changeFilterActive = () => {
+        props.changeFilter("active");
+    }
+    const changeFilterCompleted = () => {
+        props.changeFilter("completed");
+    }
+
+    const tsarChangeFilter = (filterValue: FilterValuesType) => {
+        props.changeFilter(filterValue);
+    }
+
+    const mappedTasks = props.tasks.map(t => {
+            const removeTaskHandler = () => {
+                props.removeTask(t.id)
+            }
+            return (
+                <li key={t.id}>
+                    <input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <Button name={'x'} callBack={removeTaskHandler}/>
+                </li>
+            )
+        }
+    )
     return <div>
         <h3>{props.title}</h3>
         <div>
-            <input/>
-            <button>+</button>
+            <input value={title} onKeyDown={onKeyDownHandler} onChange={onChangeHandler}/>
+            {/*<button onClick={addTaskHandler}>+</button>*/}
+            <Button name={'+'} callBack={addTaskHandler}/>
         </div>
         <ul>
-            {boxForFilters().map((el, index)=>{
-                return(
-                    <li key={el.id}>
-                        <button onClick={()=>props.removeTask(el.id, 'Hello')}>X</button>
-                        <input type="checkbox" checked={el.isDone}/>
-                        <span>{el.title}</span>
-                    </li>
-                )
-            })}
+            {mappedTasks}
         </ul>
         <div>
-            <button onClick={()=>{filterTask('All')}}>All</button>
-            <button onClick={()=>{filterTask('Active')}}>Active</button>
-            <button onClick={()=>{filterTask('Completed')}}>Completed</button>
+            <Button name={'All'} callBack={() => tsarChangeFilter('all')}/>
+            <Button name={'Active'} callBack={() => tsarChangeFilter('active')}/>
+            <Button name={'Completed'} callBack={() => tsarChangeFilter('completed')}/>
         </div>
     </div>
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type PropsType={
+    name:string
+    callBack:()=>void
+}
+export const Button = (props:PropsType) => {
+    const onClickHandler = ()=>{
+        props.callBack();
+    }
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    );
+};
